418f73506a2af8c529046cb3857fcdf9c51f1d54
Henry <henryandcorkie@gmail.com>
2024-09-03 13:02:14 -0400

    easy

--- main.py
+++ main.py
@@ -8,5 +8,16 @@ import time
 choices = [1, 2, 3, 4, 5, 6]
 
 print("|===|")
-print(f'{thing1}{thing2}{thing3}')
+print(f'|{thing1}{thing2}{thing3}|')
+print("|===|")
+
+start = input("Spin? Y/N")
+
+if start == "Y":
+    thing1 = random.choice(choices)
+    thing2 = random.choice(choices)
+    thing3 = random.choice(choices)
+
+print("|===|")
+print(f'|{thing1}{thing2}{thing3}|')
 print("|===|")
